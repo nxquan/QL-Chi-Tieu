179373f3d4c63ce18b2f4fd07e9b357444cf99b8
VTaPo <105722903+VTaPo@users.noreply.github.com>
2022-05-17 19:35:35 +0700

    Update No3Nam.h

--- No3Nam.h
+++ No3Nam.h
@@ -1,5 +1,16 @@
-#pragma once
-class No3Nam
+#ifndef NoBaNam_h
+#define NoBaNam_h
+#include "TienNo.h"
+#include <iostream>
+using namespace std;
+class NoBaNam:public TienNo
 {
+private:
+	double laiSuat;
+public:
+	NoBaNam();
+	void ganLaiXuatNam(double);
+	double tinhTongNo();
+	double tinhTongNoSauXThang(int);
 };
-
+#endif // !NoBaNam_h
