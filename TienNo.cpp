4a63224c8847dcc83aab9475313ff77060c307cb
VTaPo <105722903+VTaPo@users.noreply.github.com>
2022-05-17 19:34:47 +0700

    Update TienNo.cpp
    5/17/2022 7:32PM

--- TienNo.cpp
+++ TienNo.cpp
@@ -1,1 +1,17 @@
 #include "TienNo.h"
+
+TienNo::TienNo()
+{
+	this->tienNoGoc = 0;
+	this->ngayMuonNo = "";
+}
+
+void TienNo::ganTienNoGoc(double tienNo)
+{
+	this->tienNoGoc=tienNo;
+}
+
+void TienNo::ganNgayMuonNo(string ngayMuon)
+{
+	this->ngayMuonNo = ngayMuon;
+}
