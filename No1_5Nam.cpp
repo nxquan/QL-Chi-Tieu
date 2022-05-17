4b5af3651698b8058ed9642e592783839248f93b
VTaPo <105722903+VTaPo@users.noreply.github.com>
2022-05-17 19:38:19 +0700

    Update No1_5Nam.cpp
    5/17/2022 7:38PM

--- No1_5Nam.cpp
+++ No1_5Nam.cpp
@@ -1,1 +1,39 @@
-#include "No1_5Nam.h"
+#include "NoNamRuoi.h"
+
+NoNamRuoi::NoNamRuoi()
+{
+	this->laiSuat.resize(18);
+	for (int i = 0; i < this->laiSuat.size(); i++) {
+		this->laiSuat[i] = 0;
+	}
+}
+
+void NoNamRuoi::ganLaiSuatThang()
+{
+	for (int i = 0; i < this->laiSuat.size(); i++) {
+		cout << "Nhap lai suat thang thu " << i + 1 << ": ";
+		cin >> this->laiSuat[i];
+	}
+}
+
+double NoNamRuoi::tinhTongNo()
+{
+	double tongNo = this->tienNoGoc;
+	for (int i = 0; i < this->laiSuat.size(); i++) {
+		tongNo += tongNo * this->laiSuat[i];
+	}
+	return tongNo;
+}
+
+double NoNamRuoi::tinhTongNoSauXThang(int x)
+{
+	while (x < 0 || x>18) {
+		cout << "Thoi gian truyen vao khong hop le, nhap lai: ";
+		cin >> x;
+	}
+	double tongNo = this->tienNoGoc;
+	for (int i = 0; i < x; i++) {
+		tongNo += tongNo * this->laiSuat[i];
+	}
+	return tongNo;
+}
