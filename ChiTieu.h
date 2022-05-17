2bca671081982623b0e6c719724e6e8eeb64606f
ntclai <105714319+ntclai@users.noreply.github.com>
2022-05-17 20:07:45 +0700

    Update ChiTieu.h

--- ChiTieu.h
+++ ChiTieu.h
@@ -1,5 +1,19 @@
 #pragma once
+#ifndef ChiTieu_h
+#define ChiTieu_h
+#include<iostream>
+using namespace std;
+
 class ChiTieu
 {
+private:
+	double anUong;
+	double dienNuoc;
+	double khac;
+public:
+	ChiTieu();
+	void nhapChTieu();
+	double tinhTongChiTieu();
+	~ChiTieu();
 };
-
+#endif // !ChiTieu_h
