23dd67109dded82584fca73b49519a81d239be1e
ntclai <105714319+ntclai@users.noreply.github.com>
2022-05-17 20:11:52 +0700

    Update ThuNhap.h

--- ThuNhap.h
+++ ThuNhap.h
@@ -1,5 +1,18 @@
-#pragma once
+#define ThuNhap_h
+#include<iostream>
+using namespace std;
+
 class ThuNhap
 {
+private:
+	double luong;
+	double thuNhapKhac;
+public:
+	ThuNhap();
+	void nhapLuong();
+	void nhapThuNhapKhac();
+	double layLuong();
+	double layThuNhapKhac();
+	~ThuNhap();
 };
-
+#endif // !ThuNhap_h
