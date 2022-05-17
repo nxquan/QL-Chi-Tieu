9c52892c9f4a44ef66eba7fef1a5fdd51c41c3ad
ntclai <105714319+ntclai@users.noreply.github.com>
2022-05-17 20:43:01 +0700

    Update TKTietKiem.cpp
    Last pull

--- TKTietKiem.cpp
+++ TKTietKiem.cpp
@@ -1,1 +1,38 @@
 #include "TKTietKiem.h"
+
+TKTietKiem::TKTietKiem()
+{
+	this->tienGui = 0;
+	this->laiSuat = 0;
+	this->thoiHan = 0;
+	this->tienNhan = 0;
+}
+
+TKTietKiem::TKTietKiem(int thoiHan, double laiSuat, double tienGui)
+{
+	this->thoiHan = thoiHan;
+	this->laiSuat = laiSuat;
+	this->tienGui = tienGui;
+}
+
+void TKTietKiem::ganTienGui(double tienGui)
+{
+	this->tienGui = tienGui;
+}
+
+double TKTietKiem::layTienGui()
+{
+	return this->tienGui;
+}
+
+void TKTietKiem::ganLaiSuat(double laiSuat)
+{
+	this->laiSuat = laiSuat;
+}
+
+double TKTietKiem::tinhTienNhanDungKyHan()
+{
+	return this->tienGui * this->laiSuat + this->tienGui;
+}
+
+TKTietKiem::~TKTietKiem(){}
