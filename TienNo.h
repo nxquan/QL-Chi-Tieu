4991c63f10a72ff78883557fa1ea67a4e5b8f818
VTaPo <105722903+VTaPo@users.noreply.github.com>
2022-05-17 19:32:46 +0700

    Update TienNo.h
    5/17/2022 7:32PM

--- TienNo.h
+++ TienNo.h
@@ -1,5 +1,17 @@
-#pragma once
+#ifndef TienNo_h
+#define TienNo_h
+#include <iostream>
+using namespace std;
 class TienNo
 {
+protected:
+	double tienNoGoc;
+	string ngayMuonNo;
+public:
+	TienNo();
+	void ganTienNoGoc(double);
+	void ganNgayMuonNo(string);
+	virtual double tinhTongNo() = 0;
+	virtual double tinhTongNoSauXThang(int x) = 0;
 };
-
+#endif // !TienNo_h
