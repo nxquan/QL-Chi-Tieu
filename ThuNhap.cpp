3ac65fa5c32d75b4a6a5d26f0956d25b2bd1991d
ntclai <105714319+ntclai@users.noreply.github.com>
2022-05-17 20:48:57 +0700

    Update ThuNhap.cpp
    Last pull

--- ThuNhap.cpp
+++ ThuNhap.cpp
@@ -1,1 +1,37 @@
 #include "ThuNhap.h"
+
+ThuNhap::ThuNhap()
+{
+	this->luong = 0;
+	this->thuNhapKhac = 0;
+}
+
+ThuNhap::ThuNhap(double luong, double thuNhapKhac)
+{
+	this->luong = luong;
+	this->thuNhapKhac = thuNhapKhac;
+}
+
+void ThuNhap::nhapLuong()
+{
+	cout << "Nhap tong luong 2 vo chong: ";
+	cin >> this->luong;
+}
+
+void ThuNhap::nhapThuNhapKhac()
+{
+	cout << "Nhap tong thu nhap khac: ";
+	cin >> this->thuNhapKhac;
+}
+
+double ThuNhap::layLuong()
+{
+	return this->luong;
+}
+
+double ThuNhap::layThuNhapKhac()
+{
+	return this->thuNhapKhac;
+}
+
+ThuNhap::~ThuNhap(){}
